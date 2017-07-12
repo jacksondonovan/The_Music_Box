8887ad7b2989e389d0b8f93821a14f7267e79d79
Jackson Donovan <iinventedtwitter@gmail.com>
2017-07-12 13:14:04 -0600

    attempting to access heroku sql database

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,6 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: "postgres://gnzmxcukwaoanf:3addd753cc1060f079f5524c800d9c8248af121c3c3304b09a6c656e6573755a@ec2-50-17-217-166.compute-1.amazonaws.com:5432/d7irddktv1t3fm"
   },
 };
